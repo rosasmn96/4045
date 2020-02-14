ef416c60dee5179515e5fbd2e660c14094be062b
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-14 16:57:11 -0500

    Initial commit of Assignment0 Spring Boot project

---
+++ src/main/java/com/rosasmnit4045001/Rosasmnit4045001Application.java
@@ -0,0 +1,13 @@
+package com.rosasmnit4045001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rosasmnit4045001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rosasmnit4045001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rosasmnit4045001/rosasmnit4045001/Rosasmnit4045001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosasmnit4045001.rosasmnit4045001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rosasmnit4045001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
